e34798df3d8ea431ae6e0859e83a99e1a9b0806a
betterKaren <dupn_1025@163.com>
2019-10-16 17:53:49 -0400

    main page first ugly version

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,19 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
-// import { HeaderComponent } from './shared/components/header/header.component';
-// import {SharedModule} from "./shared/shared.module";
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    // HeaderComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    // SharedModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ui/src/app/pages/main-page/main-page.module.ts
+++ ui/src/app/pages/main-page/main-page.module.ts
@@ -2,7 +2,6 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { MainPageComponent } from './main-page.component';
-// import { SharedModule } from '../../shared/shared.module';
 // import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
