a1c3dacc2f90852d90783863d237d07fce579c83
betterKaren <dupn_1025@163.com>
2019-10-30 00:43:19 -0400

    share page router done, TODO: posts page

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from "./pages/main-page/main-page.component";
+import { SharePageComponent } from "./pages/share-page/share-page.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: 'share', component: SharePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ui/src/app/pages/share-page/share-page.component.ts
+++ ui/src/app/pages/share-page/share-page.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SharePageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
