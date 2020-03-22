7040c150c349e1f93181e331b7e691003dcdba07
betterKaren <dupn_1025@163.com>
2020-03-22 17:17:23 -0400

    update on vote, working on comment

--- backend/api/dbConnection.php
+++ backend/api/dbConnection.php
@@ -2,20 +2,23 @@
 // add response headers and the allowed methods
 header("Access-Control-Allow-Origin: *");           // allow PHP server to accept request from Angular server
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Headers: *");
+header('Access-Control-Allow-Credentials: true');
+
+error_reporting(E_ALL);
 
 // credentials will be used to connect database
 $host = 'localhost:3306';
 $username = "root";
-$password = "";
+$password = "*";
 $dbname = 'forumData';
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
 // test
 if ($conn->connect_error) {
-    die("Database Connection Failed: " . $conn->connect_error);
+    echo("Database Connection Failed: " . $conn->connect_error);
 }
 //else
-//  echo "Database Connected!!";
+// echo "Database Connected!!";
 
